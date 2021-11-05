e88bb9150ed6911978e86473fe5b66012df7914d
Dee <dgrowette@gmail.com>
2021-11-05 12:58:03 -0700

    added to the displayIssues function in single.js

--- assets/single.js
+++ assets/single.js
@@ -1,3 +1,7 @@
+var issueContianerEl = document.querySelector("#issues-container")
+
+
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc"
     
@@ -16,7 +20,33 @@ var getRepoIssues = function(repo) {
 }
 
 var displayIssues = function(issues) {
+    for (var i = 0; i < issues.length; i++) {
+        // create a link element to take users to the issue on github
+        var issueEl = document.createElement("a")
+        issueEl.classList = "list-item flex-row justify-space-between align-center"
+        issueEl.setAttribute("href", issues[i].html_url)
+        issueEl.setAttribute("target", "_blank")
 
+        // create span to hodl issue title
+        var titleEl = document.createElement("span")
+        titleEl.textContent = issues[i].title
+
+        // append to container
+        issueEl.appendChild(titleEl)
+
+        // create a type element
+        var typeEl = document.createElement("span")
+
+        // check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)"
+        } else {
+            typeEl.textContent = "(Issue)"
+        }
+
+        // append to contianer
+        issueEl.appendChild(typeEl)
+    }
 }
 
 
